bfe859f2209b373926a9971491a0b734be535667
pengdaokuan <pengdaokuan@cvte.com>
2023-10-11 22:15:08 +0800

    feature: 新增demo样例

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './app.css';
+import WithDemoWrapper from './components/WithDemoWrapper';
+import CountNumber from './simpleDemo/CountNumber';
+import InputText from './simpleDemo/InputText';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
-          <div className="container">React 前端项目搭建起来了！!!</div>
+        <Route path="/" exact>
+          <div className="container">下面将会演示简单例子的实现 !!!</div>
+          <WithDemoWrapper title="样例一：如何操作数据？">
+            <CountNumber />
+            <InputText />
+          </WithDemoWrapper>
+          {/* <Demo1 />
+          <Demo2 />
+          <Demo3 />
+          <Demo4 /> */}
+        </Route>
+        <Route path="/personal" exact>
+          {/* <Personal /> */}111
+        </Route>
+        <Route path="/notice" exact>
+          {/* <Notice /> */}222
         </Route>
       </Switch>
     </Router>
---
+++ src/simpleDemo/InputText/index.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './index.css';
+
+function InputText() {
+  const [val, setVal] = useState(0);
+  return (
+    <div className="input-text">
+      <div className="title1">2、输入内容</div>
+      <input
+        className="value1"
+        value={val}
+        onChange={(e) => setVal(e?.target?.value)}
+      />
+    </div>
+  );
+}
+
+export default InputText;
